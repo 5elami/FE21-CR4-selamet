a00089f25ce656262d1e2f116d904686fd364a3f
Selami Prizreni <freakwent.z@gmail.com>
2021-10-02 18:12:58 +0200

    responsive columns

--- java/index.js
+++ java/index.js
@@ -1,13 +1,24 @@
-let mydata = JSON.parse(cinema);
-console.log(mydata)
+let data = JSON.parse(cinema);
+console.log(data);
+console.log(typeof data);
 
-console.log(typeof mydata);
 
-for (let x of cinema) {
+
+for (let x of data) {
     document.getElementById("open").innerHTML 
-    += `<div class="col-3">
-    <img src="${x.img}" width="300"> 
+    += `<div class="col-sm-1 col-md-6 col-lg-4 mb3">
+    <img src="${x.img}" width="300vw" height="500vw"> 
     <p>${x.titel}</p> 
     <p>${x.genre}</p>
-    </div>`;
+    </div>
+    `;
  }
+
+document.getElementById("forhead").innerHTML +=
+ `   <div class="p-3 mb-2 bg-primary text-white">
+    <h1 class="text-center" >Block Cinema</h1>
+    </div>`;
+ 
+document.getElementById("foot").innerHTML +=` <div class="p-3 mb-2 bg-primary text-white">
+<h3 class="text-center" >BlockCinema</h3> 
+`
