aeb807be11ecc4c1aad91e6c548733e389a46b53
Selami Prizreni <freakwent.z@gmail.com>
2021-10-04 04:01:24 +0200

    04.01am

--- java/index.js
+++ java/index.js
@@ -6,19 +6,27 @@ console.log(typeof data);
 
 for (let x of data) {
     document.getElementById("open").innerHTML 
-    += `<div class="col-sm-1 col-md-6 col-lg-4 mb3">
+    += `
+    <div class="col-sm-12 col-md-6 col-lg-4 mb3">
     <img src="${x.img}" width="300vw" height="500vw"> 
-    <p>${x.titel}</p> 
+    <p class="">${x.titel}</p> 
     <p>${x.genre}</p>
+    <input class="btn btn-danger" type="submit" onClick="onClick()" value=${x.likes }Likes>
     </div>
+    
     `;
- }
+}
+
 
 document.getElementById("forhead").innerHTML +=
- `   <div class="p-3 mb-2 bg-primary text-white">
+ `   <div class="p-3 mb-5 bg-primary text-dark">
     <h1 class="text-center" >Block Cinema</h1>
     </div>`;
  
-document.getElementById("foot").innerHTML +=` <div class="p-3 mb-2 bg-primary text-white">
-<h3 class="text-center" >BlockCinema</h3> 
-`
+
+document.getElementById("foot").innerHTML +=
+` <div class="p-3 mb-0 bg-primary text-white">
+<h3 class="text-center">BlockCinema</h3> 
+</div>`;
+
+
