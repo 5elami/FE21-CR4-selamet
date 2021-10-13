82e3b1083bce961048b619ed1071188dd388636d
Selami Prizreni <freakwent.z@gmail.com>
2021-10-14 00:26:58 +0200

    fixed buttons

--- java/index.js
+++ java/index.js
@@ -3,7 +3,7 @@ console.log(data);
 console.log(typeof data);
 
 
-
+function bowser () {
 for (let x of data) {
     document.getElementById("open").innerHTML 
     += `
@@ -11,17 +11,48 @@ for (let x of data) {
     <img src="${x.img}" width="300vw" height="500vw"> 
     <p class="">${x.titel}</p> 
     <p>${x.genre}</p>
-    <input class="btn btn-danger" type="submit" onClick="onClick()" value=${x.likes }Likes>
+    <div class="gef btn btn-danger">${x.likes }Likes</div>
+    
+
     </div>
     
     `;
+}}
+bowser();
+function likeNcrs(cnma) {
+  data[cnma].likes += 1; 
+  document.getElementsByClassName("gef")[cnma].innerHTML = data[cnma].likes + " likes";
 }
+
+var liked = document.getElementsByClassName("gef");
+
+for (let i = 0; i < liked.length; i++) {
+  liked[i].addEventListener("click",function() {
+    likeNcrs(i);
+  });
+}
+
+
+
+
+function ordnen() {
+  data.sort((a,b) => b.likes - a.likes) ;
+  document.getElementById("open").innerHTML="";
+  bowser();
+}
+/* adding the event listener below to sort the top 
+  movies of the user comunity */
+
+document.getElementById("top").addEventListener("click",function(){
+  ordnen();
+
+})
 
 
 document.getElementById("forhead").innerHTML +=
  `   <div class="p-3 mb-5 bg-primary text-dark">
     <h1 class="text-center" >Block Cinema</h1>
-    </div>`;
+    </div> `;
  
 
 document.getElementById("foot").innerHTML +=
